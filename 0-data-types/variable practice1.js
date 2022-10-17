9c7d2022206fe350ddcaba5983d194f03df78123
nebulamarie <mepps1115@gmail.com>
2022-10-17 17:48:20 -0400

    variable assignment complete

---
+++ 0-data-types/variable practice1.js
@@ -0,0 +1,27 @@
+//https://github.com/Nebula-Academy/SEV9-Curriculum.g
+//my commit message
+//Create a set of variables that define an animal (age, height, name, etc...). Must be at least 6 variables.
+
+let name = 'kitty'
+let age = 4
+let height = '13 inches'
+let color = 'black'
+let weight = '12 pounds'
+let tail = 'white'
+console.log(name, age, height, weight, tail)
+
+
+
+
+
+//Create a set of variables that define a vehicle. Must be at least 6 variables.
+
+let Cname = 'Rogue'
+let Ccolor = 'red'
+let model = 'suv'
+let year = '2018'
+let style = 'cute'
+let tires = 'round'
+console.log(Cname, Ccolor, model, year, style, tires)
+
+

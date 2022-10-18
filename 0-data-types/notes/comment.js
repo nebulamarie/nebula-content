56face5258c8a93bd731c25aaf64ac1b12f4776d
nebulamarie <mepps1115@gmail.com>
2022-10-18 19:39:15 -0400

    homework complete

---
+++ 0-data-types/notes/comment.js
@@ -0,0 +1,14 @@
+// this is a comment
+// this is a comment
+/* dassss
+lkkkk
+pooowey
+ */
+// pseudo coding
+// code spell checker
+
+
+// pseudo coding is not an exact science
+Node
+cd curriculum
+

486bb79d49e4298939407ff6b0ddab78e1a34e60
nebulamarie <mepps1115@gmail.com>
2022-10-25 18:42:52 -0400

    strings homework complete

---
+++ 3-strings/assignments/stringsHomework.js
@@ -0,0 +1,73 @@
+
+// 1. Concatenate "hello" with "goodbye"
+ //let string1 = "hello"
+ //let string2  = "goodbye"
+ //let concatenatedstring = string1 + string2
+console.log(concatenatedstring)
+
+// 2. Save the substring "Friend" from the string "Hello Friend"
+//const greeting = "Hello Friend"
+//console.log(greeting.slice(6, 12))
+
+// 3. Using 5 or more of the following words, create a template literal that makes a legible sentence.
+//    Do not create your own words, use the existing ones below!
+
+const word1 = "hello"; 
+const word2 = "horror";
+const word3 = "peace";
+const word4 = "world"
+const word5 = "dislike"; 
+const word6 = "umbrella"; 
+const word7 = "car"; 
+const word8 = "pasta"; 
+const word9 = "pizza"; 
+const word10 = "movies";
+const word11 = "football"; 
+const word12 = "soccer"; 
+const word13 = "eat"; 
+const word14 = "cookie"; 
+const word15 = "favorite";
+const word16 = "makeup"; 
+const word17 = "I"; 
+const word18 = "to"; 
+const word19 = "my";
+const word20 = "is";
+const word21 = "and";
+const word22 = "also";
+const word23 = "like"; 
+//console.log(savedtemplateliteral=`${word1}, ${word17} ${word5} ${word9}. ${word17} ${word13} ${word8}.`)
+
+// 4. Given the string of your full name get your initials.
+let myName = 'Marie Epps'
+//console.log(myName[0, + 6])
+
+
+
+// 5. Get the 5th character from the following string: 'Hello World'
+const greeting1 = 'Hello World'
+//console.log(splicegreeting1[5])
+
+
+// 6. Find the index of the dash from 'Coding-Time!'
+const coding = 'Coding-Time!'
+//console.log(indexOf(coding('-'))
+
+// 7. Write code to grab the final letter from a any string.
+const string = "string"
+//console.log(slice.string)
+
+
+
+// 8. Write code to grab the first 3 letters from a string. 
+
+
+// 9. Turn the following string into an array of words: 'This-is-a-sentence';
+const sentence1 = 'This-is-a-sentence'
+//console.log(sentence1[ ]))
+
+// 10. Make the string of 12 from the following two numbers:
+let one = 1;
+let two = 2;
+//console.log(concat(one + two)
+
+

8bb506db0ffb9f0a3f94e498c3a43e902047eb99
nebulamarie <mepps1115@gmail.com>
2022-10-25 20:30:46 -0400

    arrays homework complete

---
+++ 5-arrays/assignments/homework.js
@@ -0,0 +1,66 @@
+// 1.   Create an array that contains two of your favorite things to do
+const myFavs = ["cook", "read" , "shop", "learn", "power tools"]
+//console.log(myFavs)
+
+// 2.   Using an array method, add another thing you like to do to that list
+//console.log(myFavs.push("play with dog"));
+//console.log(myFavs)
+
+// 3.   Reverse the order of the array (remember, if needed use MDN)
+//console.log(myFavs.reverse())
+
+//      Use this array for questions 4-11: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const dayOfwk = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+//console.log(dayOfwk)
+
+// 4.   log the length of the array
+//console.log(dayOfwk.length)
+
+// 5.   log the 4th element in the array
+//console.log(dayOfwk[4]);
+
+// 6.   Remove the first element in the array. Log the new array and the element removed from the array
+//console.log(dayOfwk.shift());
+//console.log(dayOfwk)
+
+
+// 7.   Put 'Sunday' back at the beginning of the array and log the new array
+//console.log(dayOfwk.unshift("Sunday"));
+//console.log(dayOfwk)
+
+
+// 8.   Remove the last element in the array. Log the new array and log the element removed
+//console.log(dayOfwk.pop());
+//console.log(dayOfwk)
+
+// 9.   Add 'Saturday' back to the end of the array and log the new array
+//console.log(dayOfwk.push('Saturday'));
+//console.log(dayOfwk)
+
+//10.   Replace 'Thursday' with 'Friday Junior'
+// console.log(dayOfwk)
+// console.log(dayOfwk.splice(4,1));
+// console.log(dayOfwk.slice(4))
+// console.log(dayOfwk.push("Friday Junior"));
+// console.log(dayOfwk)
+
+
+//11.   Extract your favorite day from the array and log the string: 'My favorite day of the week is (day)'
+// const dDay = (dayOfwk[6])
+// console.log(dDay)
+// //console.log('My favorite day of the week is ' + dDay )
+// console.log( `My favorite day of the week is ${dDay} `)
+
+
+// //12.   Combine these two arrays together
+// let phone = ['iphone', 'android']
+// let laptop = ['MacBook', 'HP', 'Dell']
+// const device = phone.concat(laptop);
+// console.log(device)
+
+// //13.   Write a line of code to test if something is an array or not
+// const string = "Hello World" 
+// console.log(Array.isArray(string));
+
+const kids = ['Dante, Nolan, Shelby']
+console.log(Array.isArray(kids));

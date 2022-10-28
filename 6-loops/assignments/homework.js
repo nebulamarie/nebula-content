fa1502554313ecbc361ee93ae2b26c78b5e20157
nebulamarie <mepps1115@gmail.com>
2022-10-28 19:36:10 -0400

    loop homework complete

---
+++ 6-loops/assignments/homework.js
@@ -0,0 +1,36 @@
+// // 1.   Declare a for loop that starts at 0, goes until 30, and increments by 1
+// //      Each loop it should log to the console 'the current index is _'
+
+// for (i = 0; i < 30; i++) {
+//     console.log('the current index is _', i)
+//  }
+
+
+// // // // // // 2.   Declare a for loop that starts at 1, goes until 10, and increments by 2
+// // // // // //      Each loop it should log to the console the current index
+//   for (i = 1; i < 10; i += 2) {
+//        console.log('the current index is _', i)
+//   }
+
+
+// // 3.   Create an array with your top 5 celebrity names in it. 
+// //      Be sure each element is a first & last name, ex: ['Martha Stewart', 'David Beckham', ...]
+// //      Loop over the array and log each celebrity's full name to the console.
+
+//let celebs = ['Michael Jackson', "Prince Nelson", 'Oprah Winfrey', 'Denzel Washington', 'Dwayne Johnson']
+// for (let i = 0; i <celebs.length; i++){
+//      console.log(i)
+//  console.log(celebs[i])
+//   }
+
+
+
+// 4.   Loop over your celebrity list and 
+// //      If a celebrity's full name has an even number of characters log it to the console
+let celebs = ['aa', 'bbb', 'cccc']
+ for (i =0; i <celebs.length; i++) {
+  if(celebs[i].length %2 ==0){
+     console.log(celebs[i])
+  } 
+
+  }

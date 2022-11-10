997f577bea5d0d1b470867e1a2f7c856add973c6
nebulamarie <mepps1115@gmail.com>
2022-11-10 17:35:55 -0500

    objects homewor complete

---
+++ 8-objects/assignments/homework .js
@@ -0,0 +1,95 @@
+// 1.   Create an object for your dream vehicle
+//      Give it a minimum of 6 key-value pairs
+//      This object should contain many datatypes (string, number, boolean, null, array)
+
+const  myCar =  {
+   maker: "Jaguar",
+   model: "F-Pace",
+    year: 2022,
+    colorAvail: ["green", "white" , "black"] ,
+    semiconductor: null,
+    leatherSeat:  true
+
+};
+// console.log(myCar)
+
+// 2.   Using dot-notation Log to the console 3 properties from the previous object
+// console.log(myCar.maker)
+// console.log(myCar.model)
+// console.log(myCar.year)
+
+// 3.   Using bracket-notation Log to the console 3 properties from the previous object
+// console.log([myCar.maker])
+// console.log([myCar.leatherSeat])
+// console.log([myCar.semiconductor])
+
+// 4.   Try and log a property that doesn't already exist - what output do we get?
+
+// function updatedCar () {
+//     myCar.startingPrice = "71000"
+
+// }
+// updatedCar()
+// console.log(myCar)
+
+// 5.   Add a new key-value pair to the vehicle. 
+
+//  function newFeature () {
+//      myCar.torque = 269,
+//     myCar.horsePower = 246
+
+// }
+// newFeature()
+// console.log(myCar)
+
+
+// 6.   Using bracket-notation update a property on the vehicle.
+
+// function redoSeat () {
+
+
+// myCar["leatherSeat"] = false
+
+//  }
+// redoSeat()
+// console.log(myCar)
+
+
+// 7.   Using dot-notation update a property on the vehicle. 
+
+//  function anotherUpdate () {
+
+//     myCar.startingPrice = "75000"
+// }
+// anotherUpdate ()
+// console.log(myCar)
+
+
+// 8.   Create a method for turning your vehicle on
+
+function startCar () {
+
+   start = "remote"
+   if "remote" = true
+   
+}
+startCar()
+console.log (myCar)
+
+// 9.   Create a method for turning your vehicle off
+function carOff () {
+
+    turnOff = "push"
+}
+carOff ()
+console.log (myCar)
+
+//10.   
+//      a. Check if your vehicle is on (it should be off)
+//      b. Start your vehicle
+//      c. Check if your vehicle is on (it should be on)
+//      d. Stop your vehicle
+//      e. Check if your vehicle is on (it should be on)
+
+
+

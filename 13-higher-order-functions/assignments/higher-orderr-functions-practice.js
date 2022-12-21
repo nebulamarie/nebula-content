8c6c24eb7d2076acd7d36baf4882ec8a0cee909a
nebulamarie <mepps1115@gmail.com>
2022-12-20 19:33:39 -0500

    more maps practice homework complete

---
+++ 13-higher-order-functions/assignments/higher-orderr-functions-practice.js
@@ -0,0 +1,24 @@
+// 1.   Write a higher-order function which takes in two numbers and performs a calculation with them
+//      Write the callback functions seperately 
+//          Ex: calculate(multiply, 2,4) => 8
+//          Ex: calculate(subtract, 2,4) => -2
+//          Ex: calculate(exponent, 2,4) => 16
+
+
+
+// 2.   Write a function that takes in any number of numbers and performs calculations on them
+//      Write the callback functions seperately 
+//      console.log(arguments) inside of a function to visualize your arguments
+//          Ex: calculateAll(multiply, 1,4,1,231,5) => 4620
+//          Ex: calculateAll(subtract, 2,4,5,7) => -14
+
+
+// 3.   Write a function that takes in a string and performs some form of manipulation on it
+//          Ex: modify(yeller, 'I need a nap') => I NEED A NAP!!!
+//          Ex: modify(sarcastic, 'I really like running') => I ReAlLy lIkE RuNnIng
+//          Ex: modify(code, 'There is no cake') => Th-r- -s n- c-k-
+
+// 4.   Write a function that takes in an array filled with different data types and returns each data type
+//          Ex: oneType(strings, ['I love coding', 10, undefined, Infinity, 'goosfraba', null, ['Nested Array!']]) => ['I love coding, 'goosfraba']
+//          Ex: oneType(arrays, ['I love coding', 10, undefined, Infinity, 'goosfraba', null, ['Nested Array!']]) => ['Nested Array!']
+//          Ex: oneType(nums, ['I love coding', 10, undefined, Infinity, 'goosfraba', null, ['Nested Array!']]) => [10, Infinity]
---
+++ 13-higher-order-functions/assignments/more-maps-practice.js
@@ -0,0 +1,43 @@
+// .Map Practice 
+
+// When you answer these questions I want to see your thought process in pseudo-code/comments.
+//const message = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'] 
+
+// 1. Using the .map method and the provided array of strings (above), create a new array that containing only the first letter of each string.
+// i will create a variable to hold the new string 
+// I will be using a loop to iterate through the arrany and then use the .push method to push the newly createed array into the variable
+
+// const messages = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'] 
+// let newArray =  messages.map(message => {
+//      return message[0]
+// }    
+// )
+
+// console.log(newArray)
+
+
+//2. Using the .map method and the provided array of strings (above), create a new array containing only words that are longer than 6 characters.
+// i will use 
+const messages = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'] 
+
+let newArray = messages.map(message => {
+      if (message.length >= 6)
+      return(message)
+ })
+ //console.log(newArray)
+ let filteredArray=newArray.filter(element => {
+     if (element === undefined) {
+          return false
+     
+     }
+     else {
+          return true
+     }
+ }
+     )
+console.log(filteredArray)
+
+// 3. Using the .map method and the provided array of strings (above), create a new array containing 3 strings. 
+// - The quote
+// - The year (2015)
+// - The source (Chris Pine - Learn JavaScript)

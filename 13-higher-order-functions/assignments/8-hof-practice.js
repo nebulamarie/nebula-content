9d6c2150cbb479f1453dc04c5a434997df1fa24c
nebulamarie <mepps1115@gmail.com>
2022-12-22 19:07:32 -0500

    8-hof-practice homework complete

---
+++ 13-higher-order-functions/assignments/8-hof-practice.js
@@ -0,0 +1,26 @@
+// .sort()
+
+// 1.   Write a HOF to sort an array of numbers from largest to smallest
+
+// const nums = [1,25,6,3,88,45,0]
+// nums.sort((a,b)=> a-b)
+// console.log(nums)
+
+
+// // 3.   Write a HOF to sort peoples names by their length
+// const partyPeople = [{name: 'John', age: 25}, {name: 'Jordan', age: 18}, {name: 'Frank', age: 50}, {name: 'Anna', age: 12}]
+// partyPeople.sort((a,b) => a.name.length-b.name.length)
+// console.log(partyPeople)
+
+
+// 4.   Write an HOF to sort an array of objects of people by their age youngest to oldest
+// const partyPeople = [{name: 'John', age: 25}, {name: 'Jordan', age: 18}, {name: 'Frank', age: 50}, {name: 'Anna', age: 12}]
+// partyPeople.sort((a,b) => a.age-b.age)
+// console.log(partyPeople)
+
+// 5.   Write an HOF to sort an array of strings by the last letter of the string
+const strings = ['sam', 'john', 'daniel', 'vanessa']
+strings.sort(a,b) => a.length(charAt[-1] < b.length(charAt[-1]))
+console.log(strings)
+
+///   NOT SURE WHY GETTING FAT ARROW ERROR ON THIS!!!
--- 13-higher-order-functions/assignments/higher-orderr-functions-practice.js
+++ 13-higher-order-functions/assignments/higher-orderr-functions-practice.js
@@ -4,6 +4,17 @@
 //          Ex: calculate(subtract, 2,4) => -2
 //          Ex: calculate(exponent, 2,4) => 16
 
+// const addition = (num1, num2) => num1 + num2
+// const subtraction = (num1, num2) => num1 - num2
+// const multiply = (num1, num2) => num1 * num2
+
+// const calculate = (num1, num2, callback) => {
+//     return callback(num1, num2)
+// }
+
+// //console.log(calculate(2, 4, addition))
+// //console.log(calculate(2, 4, subtraction))
+// console.log(calculate(2, 4, multiply))
 
 
 // 2.   Write a function that takes in any number of numbers and performs calculations on them
@@ -12,11 +23,43 @@
 //          Ex: calculateAll(multiply, 1,4,1,231,5) => 4620
 //          Ex: calculateAll(subtract, 2,4,5,7) => -14
 
+//let nums = [8, 4, 5, 17]
+function calculateAll(callback, ...nums){
+    return callback(nums)
+
+}
+
+
+function multiply(nums) {
+let holder =1
+for (let index = 0; index < nums.length; index++) {
+    holder = holder*nums[index];
+    
+}
+    return holder
+}
+//console.log(calculate(multiply,))
+//console.log(multiply([1,2,300]))
+//console.log(calculateAll(multiply,1,2,3))
+
+
 
 // 3.   Write a function that takes in a string and performs some form of manipulation on it
 //          Ex: modify(yeller, 'I need a nap') => I NEED A NAP!!!
 //          Ex: modify(sarcastic, 'I really like running') => I ReAlLy lIkE RuNnIng
 //          Ex: modify(code, 'There is no cake') => Th-r- -s n- c-k-
+
+let str = 'I need a nap' 
+const yeller = (str) => {
+     return str.toUpperCase() + '!!!'
+ }
+ //console.log(yeller(str))
+ 
+ function modify(callback, string){
+    return callback(string)
+
+}
+console.log(modify(yeller, str))
 
 // 4.   Write a function that takes in an array filled with different data types and returns each data type
 //          Ex: oneType(strings, ['I love coding', 10, undefined, Infinity, 'goosfraba', null, ['Nested Array!']]) => ['I love coding, 'goosfraba']
